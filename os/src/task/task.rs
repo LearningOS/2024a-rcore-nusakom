a3de0fd9d8092f0d97dda141932dae7c1075ae84
nusakom <2031263745@qq.com>
2024-10-12 21:12:14 +0800

    10/12

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -1,16 +1,18 @@
+//! Types related to task management
+
 use super::TaskContext;
-use std::time::{SystemTime, UNIX_EPOCH};
+use crate::config::MAX_SYSCALL_NUM;
 
 /// The task control block (TCB) of a task.
 #[derive(Copy, Clone)]
 pub struct TaskControlBlock {
-    /// The task status in its lifecycle
+    /// The task status in it's lifecycle
     pub task_status: TaskStatus,
     /// The task context
     pub task_cx: TaskContext,
-    /// syscall times
-    pub syscall_times: [u32; MAX_SYSCALL_NUM],
-    /// first run time
+    /// The task syscall times
+    pub task_syscall_times: [u32; MAX_SYSCALL_NUM],
+    /// The task time
     pub task_time: usize,
 }
 
